5ef26daccf5fd90b7c7898370f4c6f6469d5d6eb
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2022-09-27 09:11:57 +0600

    27_09_2022

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    rand = random.randint(1, 1001)
+    return f"<h1>{rand}</h1>"
+
+
+# Запуск:
+# set FLASK_APP=main
+# flask run
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
